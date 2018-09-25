094f4e214e9f5a7d079dee53d4a1927d29e450aa
Lucas <lucas.emanuel@approx.com.br>
2018-09-25 16:12:22 -0300

    UpdateLucas
    Pagina Pronta. Validações concluidas, fazendo ajustes na responsividade.

--- resources/views/Registration/create.blade.php
+++ resources/views/Registration/create.blade.php
@@ -1,5 +1,9 @@
+
 @extends('layouts.root')
+
 @section('content')
+
+
 
 {!!Form::open(array('url'=>'clients', 'method'=>'POST', 'autocomplete'=>'off', 'role' => 'search'))!!}
     
@@ -17,42 +21,50 @@
             </ul>
         @endif()
     </div>
-    <div class="form-group input-group mb-3">
-        <div  class="form-group">
-            <input type="text" class="form-group" id="nome" name="name" value="{{ old('name') }}" placeholder="DIGITE SEU NOME">
-        </div>
-        <div>
-            <input type="text" class="form-group" id="registration" name="registration" value="{{ old('registration') }}" placeholder="DIGITE SUA MATRICULA">
-        </div>
-        <div class="row">
+    <div class="">
+        <div class="form-group justify-content-center">
+            <div  class="form-group">
+                <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}" placeholder="DIGITE SEU NOME">
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" id="registration" name="registration" value="{{ old('registration') }}" placeholder="DIGITE SUA MATRICULA">
+            </div>
+            <div class="row form-group">
+                
+                <div class="col-6">
+                    {!! Form::select('location',['' => 'SELECIONE SUA UNIDADE', 'MG' => 'MG', 'SP' => 'SP', 'RJ' => 'RJ' ] , ['placeholder' => 'awesome']);!!}
+                    {{-- <select name="" id="location" name="location" value="{{ old('location') }}" placeholder="SELECIONE SUA UNIDADE">
+                            <option selected>Escolha...</option>
+                            <option value="MG" {{old('location') == "MG" ?'selected="selected"': ''}}>MG</option>
+                            <option value="SP" name="location">SP</option>
+                            <option value="RJ" name="location">RJ</option>
+                    </select> --}}
+                    {{-- <input type="select" id="shirtSize" name="shirtSize" value="{{ old('shirtSize') }}" placeholder="TAMANHO DA SUA CAMISA"> --}}
+                </div>
+                <div class="col-6">
+                    <div class="">
+                        {!! Form::select('shirtSize',['' => 'TAMANHO DA SUA CAMISA', 'M' => 'M', 'G' => 'G', 'GG' => 'GG' ]);!!}
+                    </div>
+                        
+                    {{-- <select name="" id="shirtSize" name="shirtSize" value="{{ old('shirtSize') }}" placeholder="SELECIONE SUA UNIDADE">
+                            <option selected>Escolha...</option>
+                            <option value="P"> P</option>
+                            <option value="M"> M</option>
+                            <option value="G"> G</option>
+                            <option value="GG">GG</option>
+                    </select> --}}
+                    {{-- <input type="text" id="location" name="location" value="{{ old('location') }}" placeholder="TAMANHO DA SUA CAMISA"> --}}
+                </div>
+            </div>
+            <div>
+                <p><b>DIGITE 1(UMA) PALAVRA QUE REPRESENTE O SEU SENTIMENTO DE ESTAR NO GRUPO MOPAR.</b></p>
+            </div>
             
-            <div class="col-6">
-                {!! Form::select('location',['' => 'Escolha', 'MG' => 'MG', 'SP' => 'SP', 'RJ' => 'RJ' ]);!!}
-                {{-- <select name="" id="location" name="location" value="{{ old('location') }}" placeholder="SELECIONE SUA UNIDADE">
-                        <option selected>Escolha...</option>
-                        <option value="MG" {{old('location') == "MG" ?'selected="selected"': ''}}>MG</option>
-                        <option value="SP" name="location">SP</option>
-                        <option value="RJ" name="location">RJ</option>
-                </select> --}}
-                {{-- <input type="select" id="shirtSize" name="shirtSize" value="{{ old('shirtSize') }}" placeholder="TAMANHO DA SUA CAMISA"> --}}
+            <div class="form-group">
+                <input type="text" class="form-control" id="word" name="word" value="{{ old('word') }}" placeholder="DIGITE UMA PALAVRA QUE REPRESENTA SEU SENTIMENTO COM A MOPAR">
             </div>
-            <div class="col-6">
-                    {!! Form::select('shirtSize',['' => 'Escolha', 'M' => 'M', 'G' => 'G', 'GG' => 'GG' ]);!!}
-                {{-- <select name="" id="shirtSize" name="shirtSize" value="{{ old('shirtSize') }}" placeholder="SELECIONE SUA UNIDADE">
-                        <option selected>Escolha...</option>
-                        <option value="P"> P</option>
-                        <option value="M"> M</option>
-                        <option value="G"> G</option>
-                        <option value="GG">GG</option>
-                </select> --}}
-                {{-- <input type="text" id="location" name="location" value="{{ old('location') }}" placeholder="TAMANHO DA SUA CAMISA"> --}}
-            </div>
-        </div>
-        <p><b>DIGITE UMA PALAVRA QUE REPRESENTE O SEU SENTIMENTO DE ESTAR NO GRUPO MOPAR.</b></p>
-        <div>
-            <input type="text" class="form-group" id="word" name="word" value="{{ old('word') }}" placeholder="DIGITE UMA PALAVRA QUE REPRESENTA SEU SENTIMENTO COM A MOPAR">
-        </div>
-        <button type="submit">ENVIAR PALAVRA</button>
+            <button type="submit" class="btn btn-custom">ENVIAR PALAVRA</button>        </div>
     </div>
+    
 {{Form::close()}}
 @stop
--- resources/views/layouts/root.blade.php
+++ resources/views/layouts/root.blade.php
@@ -15,35 +15,46 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
     <link href="{{ asset('css/root.css') }}" rel="stylesheet">
 
     {{-- Bootstrap --}}
+    
     
     {{-- background-image --}}
     <style>
         background-image:url({{url('images/hotsite-03.jpg')}});
     </style> 
 </head>
+<link href="{{ asset('css/root.css') }}" rel="stylesheet">
 <body>
-    <div id="app" class="container-root">
-        <div class="h1-root row">
+    <div id="app" class="container-fluid container-root">
+        <div class="h1-root row col-12">
             <div class="col-8">
-                <h1><p>PRA VOCÊ</p><p><b>O QUE SIGNIFICA</b></p><P>A MOPAR</P></h1>
+                <h1><p>PRA VOCÊ</p><p><b>O QUE SIGNIFICA</b></p><P>A MOPAR?</P></h1>
             </div>
-            <div class="col4">
-                <img src="public/images/itens1.png" alt="">
+            <div class="col-4">
+                <div>
+                    <img id="image1" src="images/itens1.png" alt="">
+                </div>
             </div>
             
         </div>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
+        <div class="row">
+            <main class="py-4 col col-6">
+                @yield('content')
+            </main>
+            <div class="col-6">
+                <img id="image2" src="images/branca.png" alt="">
+            </div>
+        </div>
+        
+        
         
     </div>
 </body>
